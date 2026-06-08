1778d430f76c6d8db3cef9c944456ac666041289
kranthikumarmyakala <kranthimkkl@gmail.com>
2026-06-08 09:02:38 +0000

    CrossEntrophy

--- Task.py
+++ Task.py
@@ -1,21 +1,35 @@
-import pandas as pd
-import numpy as np
+# from sklearn.datasets import make_classification
+# from sklearn.model_selection import train_test_split
+# import numpy as np
+# import tensorflow as tf
+# import matplotlib.pyplot as plt
 
-#Create 4 Neurons
-inputs = [1,2,3, 2.5]
-weights = [[0.2,0.8,-0.5,1],
-           [0.5,-0.91,0.26,-0.5],
-           [-0.26,-0.27,0.17,0.87],
-           [0.1, -0.14, 0.5, -0.5]]
-bias = [2,3,0.5, 0.1]
+# #splitting the data into train and test sets
+# x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=0.2, random_state=2022)
 
-# for loop to dot product of inputs and weights and add bias
-outputs = []
-for neuron_weights, neuron_bias in zip(weights, bias):
-    neuron_output = 0
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input*weight
-    neuron_output += neuron_bias
-    outputs.append(neuron_output)
+# #print the train and test split shapes
+# print("X train set: ", x_train.shape, "Xtest set: ", x_test.shape,
+#       "y train set: ", y_train.shape, "y test set: ", y_test.shape)
 
-print(outputs)
+# x,y = make_classification(n_samples=10000, n_informative=10, random_state=2022)
+
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(10, activation="relu", input_shape=(x_train.shape[1],)),
+#     tf.keras.layers.Dense(10, activation="relu"),
+#     tf.keras.layers.Dense(1, activation="sigmoid")
+# ])
+
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# history = model.fit(x_train, y_train, epochs=20, validation_data=(x_test, y_test), verbose=1)
+
+# #plot the loss of the models
+# fig, ax = plt.subplots(figsize=(12, 6))
+# plt.plot(history.history['loss'], label='train loss')
+# plt.plot(history.history['val_loss'], label='test loss')
+# plt.title('Loss')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.xticks(np.arange(0, 20, 1))
+# plt.legend(['train loss', 'test loss'], loc='upper right')
+# plt.show()
